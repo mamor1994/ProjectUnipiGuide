373723efa70bbc6e348a6e086f42b8c211c683c6
mppl2205 <mppl2205@unipi.gr>
2023-09-12 04:38:57 +0300

    Add gif in the byebye page

---
+++ ProjectUnipiGuide/ByeByePage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectUnipiGuide
+{
+    public partial class ByeByePage : Form
+    {
+        public ByeByePage()
+        {
+            InitializeComponent();
+        }
+
+        private void ByeByePage_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.gif; 
+            ((System.ComponentModel.ISupportInitialize)(pictureBox1)).EndInit();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void goBackToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            EndPage form = new EndPage();
+            form.ShowDialog();
+            this.Hide();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string message = "Εδώ θα βρείτε έναν οδηγό για το Πανεπιστήμιο Πειραιώς η οποία δημιουργήθηκε από τον Αντώνη Τζιβάκη και την Μαρία Αμοργιανού.";
+            MessageBox.Show(message, "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
--- ProjectUnipiGuide/Program.cs
+++ ProjectUnipiGuide/Program.cs
@@ -16,7 +16,7 @@ namespace ProjectUnipiGuide
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new EntrancePage());
+            Application.Run(new ByeByePage());
         }
     }
 }
