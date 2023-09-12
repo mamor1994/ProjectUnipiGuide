bb9b4b45d3be3d889112957259e4b0883ae33549
mppl2205 <mppl2205@unipi.gr>
2023-09-12 03:31:48 +0300

    Add web informatics page

--- ProjectUnipiGuide/Program.cs
+++ ProjectUnipiGuide/Program.cs
@@ -16,7 +16,7 @@ namespace ProjectUnipiGuide
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new VideoPlPage());
+            Application.Run(new WebPlPage());
         }
     }
 }
---
+++ ProjectUnipiGuide/WebPlPage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectUnipiGuide
+{
+    public partial class WebPlPage : Form
+    {
+        public WebPlPage()
+        {
+            InitializeComponent();
+        }
+
+        private void WebPlPage_Load(object sender, EventArgs e)
+        {
+            webBrowser1.ScriptErrorsSuppressed = true;
+            webBrowser1.Navigate("https://www.cs.unipi.gr/index.php?lang=el");
+        }
+
+        private void goBackToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //EntrancePage form = new EntrancePage();
+            //form.Show();
+            this.Hide();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string message = "Εδώ θα βρείτε έναν οδηγό για το Πανεπιστήμιο Πειραιώς η οποία δημιουργήθηκε από τον Αντώνη Τζιβάκη και την Μαρία Αμοργιανού.";
+            MessageBox.Show(message, "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
