8d87a045af5b990244a9fb303688a6341b3f1dfd
mppl2205 <mppl2205@unipi.gr>
2023-09-26 12:45:55 +0300

    fix an issue close #92

---
+++ ProjectUnipiGuide/Models/ApplicationSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ProjectUnipiGuide.Models.Aplication;
+
+namespace ProjectUnipiGuide.Models
+{
+    internal class ApplicationSettings
+    {
+        public Connectionstrings ConnectionStrings { get; set; }
+
+    }
+}
--- ProjectUnipiGuide/Properties/Resources.Designer.cs
+++ ProjectUnipiGuide/Properties/Resources.Designer.cs
@@ -341,6 +341,15 @@ namespace ProjectUnipiGuide.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream upliftingsound {
+            get {
+                return ResourceManager.GetStream("upliftingsound", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap αρχείο_λήψης {
