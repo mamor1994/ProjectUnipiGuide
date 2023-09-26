76cf51c21ef364557629314380759c3f225f8fd2
mppl2205 <mppl2205@unipi.gr>
2023-09-27 01:22:30 +0300

    Add redirection close #101

--- ProjectUnipiGuide/Schools.cs
+++ ProjectUnipiGuide/Schools.cs
@@ -56,11 +56,17 @@ namespace ProjectUnipiGuide
 
         private void goBackToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            UserState.IsGuest = true;
             MainPage form = new MainPage();
-            form.btnCalendar.Visible = false;
             form.Show();
             this.Hide();
+            if (UserState.IsGuest == true)
+            {
+                form.btnCalendar.Visible = false;
+            }
+            else
+            {
+                form.btnCalendar.Visible = true;
+            }
         }
 
         private void Schools_Load(object sender, EventArgs e)
