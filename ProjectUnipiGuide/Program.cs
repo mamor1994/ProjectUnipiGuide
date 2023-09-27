4554ea50599b54b3f1390c4e5dfa0be529089e35
mppl2205 <mppl2205@unipi.gr>
2023-09-27 16:24:42 +0300

    add final close #118

--- ProjectUnipiGuide/Program.cs
+++ ProjectUnipiGuide/Program.cs
@@ -17,7 +17,7 @@ namespace ProjectUnipiGuide
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new UserReviewPage());
+            Application.Run(new HelloPage());
         }
     }
 }
